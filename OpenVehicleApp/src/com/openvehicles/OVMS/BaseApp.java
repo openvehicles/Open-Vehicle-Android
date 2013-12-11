f3199198a630ba0c2131f2693729ccfbbf44a71a
Mark Webb-Johnson <mark@webb-johnson.net>
2013-12-11 17:37:18 +0800

    TestflightApp integration, and production build for v3.2.1

--- OpenVehicleApp/src/com/openvehicles/OVMS/BaseApp.java
+++ OpenVehicleApp/src/com/openvehicles/OVMS/BaseApp.java
@@ -1,6 +1,7 @@
 package com.openvehicles.OVMS;
 
 import android.app.Application;
+import com.testflightapp.lib.TestFlight;
 
 public class BaseApp extends Application {
 //	private static final String TAG = "BaseApp";
@@ -10,6 +11,7 @@ public class BaseApp extends Application {
 	public void onCreate() {
 		super.onCreate();
 		sInstance = this;
+		TestFlight.takeOff(this, "e34e1b78-366e-49b4-bd0f-ec704a76213e");
 	}
 	
 	public static BaseApp getApp() {
