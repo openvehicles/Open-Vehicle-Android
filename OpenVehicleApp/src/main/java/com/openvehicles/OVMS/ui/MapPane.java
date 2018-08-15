b25ce49ad11cbbeb24adce95486fe3158ea2d0d2
Glyn Hudson <glyn.hudson@gmail.com>
2018-08-16 00:11:52 +0100

    Target android 8 sdk fixes #66
    Update play services  and gms to use new google maps api https://mapsplatform.googleblog.com/2016/06/marker-zindex-and-more-come-to-google.html

--- OpenVehicleApp/src/main/java/com/openvehicles/OVMS/ui/MapPane.java
+++ OpenVehicleApp/src/main/java/com/openvehicles/OVMS/ui/MapPane.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -12,14 +13,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.openvehicles.OVMS.R;
 
-public class MapPane extends FragmentActivity {
-
+public class MapPane extends FragmentActivity implements OnMapReadyCallback {
+	private GoogleMap mMap;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_activity);
-		GoogleMap map = ((SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map)).getMap();
+		((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap map) {
+		mMap = map;
 
 		LatLng mapCenter = new LatLng(41.889, -87.622);
 
