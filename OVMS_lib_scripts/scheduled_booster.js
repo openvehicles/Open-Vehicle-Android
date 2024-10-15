b7e569d1a83e5ad0ce013dd8554d0591758f215b
ZorG <100448801+zorgms@users.noreply.github.com>
2024-10-15 20:53:02 +0200

    Update scheduled_booster.js
    updated to for two times booster at time

--- OVMS_lib_scripts/scheduled_booster.js
+++ OVMS_lib_scripts/scheduled_booster.js
@@ -12,17 +12,19 @@ if(init_usrcfg == "no"){
     OvmsConfig.Set("usr", "b.init", "yes");           // init usr config
     OvmsConfig.Set("usr", "b.activated", "no");       // schedule time activated     
     OvmsConfig.Set("usr", "b.scheduled", "0515");     // schedule time format 1400 for 14:00 / 2 p.m. o'clock 
+    OvmsConfig.Set("usr", "b.scheduled_2", "no");     // double schedule time for 10 min.
     OvmsConfig.Set("usr", "b.week", "no");            // schedule time weekly at Day de/activated
     OvmsConfig.Set("usr", "b.day_start", "1");        // Day1 -> Monday
     OvmsConfig.Set("usr", "b.day_end", "6");          // Day5 -> Friday + 1 switched at midnight
     OvmsConfig.Set("usr", "b.ticker", "10");     	  // ticker format 1/10/60/300/600/3600 seconds
-    OvmsConfig.Set("usr", "b.data", "0,0,0,0,-1,-1"); // Data Array
+    OvmsConfig.Set("usr", "b.data", "0,0,0,0,-1,-1,-1"); // Data Array
 }
 
 var state = {
     start_day: false,       // start_day subscription
     end_day: false,         // end_day subscription
     scheduled_boost: false, // scheduled_boost subscription
+    scheduled_boost_2: false, // scheduled_boost_2 subscription, boost two times
     booster_data: false,    // booster_data subscription
 };
 
@@ -33,8 +35,9 @@ var scheduled_ticker = 'ticker.' + OvmsConfig.Get("usr","b.ticker","30");
 function veh_on() {
     return OvmsMetrics.Value("v.e.on");
 }
-function charging() {
-    return OvmsMetrics.Value("v.c.charging");
+
+function veh_hvac() {
+    return OvmsMetrics.Value("v.e.hvac");
 }
 
 function start_day() {
@@ -48,16 +51,26 @@ function end_day() {
         OvmsConfig.Set("usr", "b.activated", "no");
     }
 }
-
+// scheduled booster time
 function scheduled_boost() {
-    if((!veh_on() && !charging()) && (OvmsConfig.Get("usr","b.activated") == "yes")) {
+    if(!veh_on() && !veh_hvac() && (OvmsConfig.Get("usr","b.activated") == "yes")) {
+        OvmsVehicle.ClimateControl("on");
+        if (OvmsConfig.Get("usr","b.scheduled_2") == "yes") {
+            state.scheduled_boost_2 = PubSub.subscribe('ticker.60',scheduled_boost_2);
+        }
+    }
+}
+// scheduled booster time add a second booster time for 10 min. booster
+function scheduled_boost_2() {
+    if(!veh_on() && !veh_hvac()) {
         OvmsVehicle.ClimateControl("on");
     }
+    PubSub.unsubscribe(state.scheduled_boost_2);
 }
 
 function booster_data() {
 
-    var newdata = OvmsConfig.Get("usr", "b.data", "0,0,0,0,-1,-1").split(",");
+    var newdata = OvmsConfig.Get("usr", "b.data", "0,0,0,0,-1,-1,0").split(",");
     if (newdata[0] == 1) {
         // booster scheduled on
         if (newdata[1] == 1) {
@@ -113,12 +126,22 @@ function booster_data() {
             var DayEnd = 'clock.day' + scheduled_end;
             state.end_day = PubSub.subscribe(DayEnd,end_day);
         }
-        OvmsConfig.Set("usr", "b.data", "0,0,0,0,-1,-1");
+        // double booster time
+        if (newdata[6] == 1) {
+            OvmsConfig.Set("usr", "b.scheduled_2", "yes");
+        }
+        // single booster time
+        if (newdata[6] == 0) {
+            OvmsConfig.Set("usr", "b.scheduled_2", "no");
+        }
+        OvmsConfig.Set("usr", "b.data", "0,0,0,0,-1,-1,-1");
     }
 
     if((!state.scheduled_boost)&&(OvmsConfig.Get("usr","b.activated") == "yes")){
         state.scheduled_boost = PubSub.subscribe('clock.0001',veh_on);
         PubSub.unsubscribe(state.scheduled_boost);
+        state.scheduled_boost_2 = PubSub.subscribe('clock.0001',veh_on);
+        PubSub.unsubscribe(state.scheduled_boost_2);
         var ClockEvent = 'clock.' + OvmsConfig.Get("usr","b.scheduled");
         state.scheduled_boost = PubSub.subscribe(ClockEvent,scheduled_boost);
     }
